fe78e0244c24d0b3f097f5c4777ac7ac190f6d17
rashmigmr13-eng <rashmigmr13@gmail.com>
2026-08-27 22:42:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_internet_gateway" "my_igw" {
 resource "aws_subnet" "my_subnet" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-west-2"
+  availability_zone       = "us-west-2a"
   map_public_ip_on_launch = true
 
   tags = {
